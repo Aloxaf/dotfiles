3f991e04d642fa5604fd9661805a26cef4b8e22d
Aloxaf <aloxafx@gmail.com>
2021-03-05 13:05:21 +0800

    [zsh] improve alias-tips

--- zsh/.config/zsh/plugins/alias-tips/alias-tips.plugin.zsh
+++ zsh/.config/zsh/plugins/alias-tips/alias-tips.plugin.zsh
@@ -1,10 +1,10 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz ${0:h}/{expand_alias,init_alias_list,_check_alias,_show_alias_tips}
+autoload -Uz ${0:h}/_altp_*
 autoload -Uz add-zsh-hook
 
-add-zsh-hook preexec _check_alias
-add-zsh-hook precmd  _show_alias_tips
+add-zsh-hook preexec _altp_check
+add-zsh-hook precmd  _altp_show_tips
 
 typeset -g ALIAS_TIPS_BUFFER
